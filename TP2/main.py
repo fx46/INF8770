48572c230eccadc98adb3c7e7cbf6e53545c76b3
fx46 <31199346+fx46@users.noreply.github.com>
2020-10-30 13:47:30 -0400

    DWT work in progress

--- TP2/main.py
+++ TP2/main.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pywt
+import cv2
 
 def convertImageToYUV(img_rgb):
     YUV = img_rgb
@@ -18,13 +20,39 @@ def convertImageToYUV(img_rgb):
 
 img_original = plt.imread('dank_luigi.jpeg').astype('int')
 
-fig, (im1, im2) = plt.subplots(1,2)
+fig, (im) = plt.subplots(1,5)
 
-im1.imshow(img_original)
-im1.set_title("Image originale RGB")
+im[0].imshow(img_original)
+im[0].set_title("Image originale RGB")
 
 img_YUV = convertImageToYUV(img_original)
-im2.imshow(img_YUV)
-im2.set_title("Image YUV")
+im[1].imshow(img_YUV)
+im[1].set_title("Image YUV")
+
+cAr, (cHr, cVr, cDr) = pywt.dwt2(img_YUV[:, :, 0], 'haar')
+cAg, (cHg, cVg, cDg) = pywt.dwt2(img_YUV[:, :, 1], 'haar')
+cAb, (cHb, cVb, cDb) = pywt.dwt2(img_YUV[:, :, 2], 'haar')
+
+H = np.zeros((178,237,3))
+V = np.zeros((178,237,3))
+D = np.zeros((178,237,3))
+
+H[:,:,0] = cHr
+H[:,:,1] = cHg
+H[:,:,2] = cHb
+V[:,:,0] = cVr
+V[:,:,1] = cVg
+V[:,:,2] = cVb
+D[:,:,0] = cDr
+D[:,:,1] = cDg
+D[:,:,2] = cDb
+
+im[2].imshow(H)
+im[2].set_title("Horizontal")
+im[3].imshow(V)
+im[3].set_title("Vertical")
+im[4].imshow(D)
+im[4].set_title("Diagonal")
+
 
 plt.show()
