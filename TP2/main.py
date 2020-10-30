c67d63ec15878784edc6521ae74c7deb4836e1b6
fx46 <31199346+fx46@users.noreply.github.com>
2020-10-30 14:18:10 -0400

    Adding approx coeff

--- TP2/main.py
+++ TP2/main.py
@@ -18,9 +18,36 @@ def convertImageToYUV(img_rgb):
 
     return YUV
 
-img_original = plt.imread('dank_luigi.jpeg').astype('int')
+def DWT(img_YUV):
+    cAr, (cHr, cVr, cDr) = pywt.dwt2(img_YUV[:, :, 0], 'haar')
+    cAg, (cHg, cVg, cDg) = pywt.dwt2(img_YUV[:, :, 1], 'haar')
+    cAb, (cHb, cVb, cDb) = pywt.dwt2(img_YUV[:, :, 2], 'haar')
 
-fig, (im) = plt.subplots(1,5)
+    A = np.zeros((len(cAr), len(cAr[0]), 3))
+    H = np.zeros((len(cHr), len(cHr[0]), 3))
+    V = np.zeros((len(cVr), len(cVr[0]), 3))
+    D = np.zeros((len(cDr), len(cDr[0]), 3))
+
+    A[:,:,0] = cAr
+    A[:,:,1] = cAg
+    A[:,:,2] = cAb
+    H[:,:,0] = cHr
+    H[:,:,1] = cHg
+    H[:,:,2] = cHb
+    V[:,:,0] = cVr
+    V[:,:,1] = cVg
+    V[:,:,2] = cVb
+    D[:,:,0] = cDr
+    D[:,:,1] = cDg
+    D[:,:,2] = cDb
+
+    im[0].imshow(A)
+
+    return A, H, V, D
+
+
+img_original = plt.imread('dank_luigi.jpeg').astype('int')
+fig, (im) = plt.subplots(1,6)
 
 im[0].imshow(img_original)
 im[0].set_title("Image originale RGB")
@@ -29,30 +56,15 @@ img_YUV = convertImageToYUV(img_original)
 im[1].imshow(img_YUV)
 im[1].set_title("Image YUV")
 
-cAr, (cHr, cVr, cDr) = pywt.dwt2(img_YUV[:, :, 0], 'haar')
-cAg, (cHg, cVg, cDg) = pywt.dwt2(img_YUV[:, :, 1], 'haar')
-cAb, (cHb, cVb, cDb) = pywt.dwt2(img_YUV[:, :, 2], 'haar')
+A, H, V, D = DWT(img_YUV)
 
-H = np.zeros((178,237,3))
-V = np.zeros((178,237,3))
-D = np.zeros((178,237,3))
-
-H[:,:,0] = cHr
-H[:,:,1] = cHg
-H[:,:,2] = cHb
-V[:,:,0] = cVr
-V[:,:,1] = cVg
-V[:,:,2] = cVb
-D[:,:,0] = cDr
-D[:,:,1] = cDg
-D[:,:,2] = cDb
-
-im[2].imshow(H)
-im[2].set_title("Horizontal")
-im[3].imshow(V)
-im[3].set_title("Vertical")
-im[4].imshow(D)
-im[4].set_title("Diagonal")
-
+im[2].imshow(A)
+im[2].set_title("Approximation Coeff")
+im[3].imshow(H)
+im[3].set_title("Horizontal")
+im[4].imshow(V)
+im[4].set_title("Vertical")
+im[5].imshow(D)
+im[5].set_title("Diagonal")
 
 plt.show()
