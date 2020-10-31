62393568fc6848fd7fc8dab6c4cc7c76ef858a8f
fx46 <31199346+fx46@users.noreply.github.com>
2020-10-31 05:38:52 -0400

    sous echantillonnage semble marcher

--- TP2/main.py
+++ TP2/main.py
@@ -2,30 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 import pywt
+import os
 
 def sousEchantillonnage(img_yuv):
-    width = len(img_yuv)
-    height = len(img_yuv[0])
-    Y = [[0]*width]*height
-    U = [[0]*(width/2)]*(height/2)
-    V = [[0]*(width/2)]*(height/2)
-    for column in range(width):
-        if column%2 == 0:
-            for row in range(height):
-                pixel = img_yuv[column][row]
-                Y[column][row] = pixel[0]
-                if row % 2 == 0:
-                    U[column/2][row/2] = pixel[1]
-                    V[column/2][row/2] = pixel[2]
-        else:   
-            for row in range(height):
-                pixel = img_yuv[column][row]
-                Y[column][row] = pixel[0]
-            
-    print(Y.size())
-    print(V.size())
-    print(U.size())
-    return Y, U, V
+    # downsample to 4 : 2 : 0 format
+    y = img_YUV[:, :, 0]
+    u = img_YUV[:, :, 1][::2]
+    v = img_YUV[:, :, 2][::2]
+
+    return y, u, v
 
 def convertImageToYUV(img_rgb):
     YUV = img_rgb
@@ -42,52 +27,37 @@ def convertImageToYUV(img_rgb):
 
     return YUV
 
-def convertImageToRGB(img_yuv):
-    RGB = img_yuv
-    width = len(img_yuv)
-    height = len(img_yuv[0])
+def convertImageToRGB(y, u, v):
+    width = len(y)
+    height = len(y[0])
+    RGB = []
     for column in range(width):
+        RGB.append([])
         for row in range(height):
-            pixel = img_yuv[column][row]
-            G = np.clip(pixel[0] - (pixel[1] + pixel[2]) / 4, 0, 255)
-            R = np.clip(pixel[2] + G, 0, 255)
-            B = np.clip(pixel[1] + G, 0, 255)
-            RGB[column][row] = [R, G, B]
+            G = np.clip(y[column][row] - (u[column//2][row] + v[column//2][row]) / 4, 0, 255).astype(np.uint8)
+            R = np.clip(v[column//2][row] + G, 0, 255).astype(np.uint8)
+            B = np.clip(u[column//2][row] + G, 0, 255).astype(np.uint8)
+            RGB[column].append([R, G, B])
 
     return RGB
 
-def DWT(img_YUV):
-    cAy, (cHy, cVy, cDy) = pywt.dwt2(img_YUV[:, :, 0], 'haar')
-    cAu, (cHu, cVu, cDu) = pywt.dwt2(img_YUV[:, :, 1], 'haar')
-    cAv, (cHv, cVv, cDv) = pywt.dwt2(img_YUV[:, :, 2], 'haar')
+def DWT(y, u, v):
+    cAy, (cHy, cVy, cDy) = pywt.dwt2(y, 'haar')
+    cAu, (cHu, cVu, cDu) = pywt.dwt2(u, 'haar')
+    cAv, (cHv, cVv, cDv) = pywt.dwt2(v, 'haar')
 
-    A = np.zeros((len(cAy), len(cAy[0]), 3))
-    H = np.zeros((len(cHy), len(cHy[0]), 3))
-    V = np.zeros((len(cVy), len(cVy[0]), 3))
-    D = np.zeros((len(cDy), len(cDy[0]), 3))
-
-    A[:,:,0] = cAy
-    A[:,:,1] = cAu
-    A[:,:,2] = cAv
-    H[:,:,0] = cHy
-    H[:,:,1] = cHu
-    H[:,:,2] = cHv
-    V[:,:,0] = cVy
-    V[:,:,1] = cVu
-    V[:,:,2] = cVv
-    D[:,:,0] = cDy
-    D[:,:,1] = cDu
-    D[:,:,2] = cDv
-
-    return A, H, V, D
+    return cAy, cAu, cAv
 
 ##############################################################################################
 # Load image
 ##############################################################################################
-DWT_recursion_level = 3
+DWT_recursion_level = 1
+image_path = 'dank_luigi.jpeg'
 
-img_original = plt.imread('dank_luigi.jpeg').astype('int')
-fig, (im) = plt.subplots(DWT_recursion_level + 1, 4)
+img_original = plt.imread(image_path).astype('int')
+longueurOriginale = os.stat(image_path).st_size * 8
+
+fig, (im) = plt.subplots(2, 4)
 
 im[0][0].imshow(img_original)
 im[0][0].set_title("Image originale RGB")
@@ -99,38 +69,39 @@ img_YUV = convertImageToYUV(img_original)
 im[0][1].imshow(img_YUV)
 im[0][1].set_title("Image YUV")
 
+y = img_YUV[:, :, 0]
+u = img_YUV[:, :, 1]
+v = img_YUV[:, :, 2]
+
 ##############################################################################################
 # Sous echantillonnage
 ##############################################################################################
-#sousEchantillonnage(img_YUV.copy())
+y, u, v = sousEchantillonnage(img_YUV.copy())
 
 ##############################################################################################
 # DWT
 ##############################################################################################
-A = img_YUV.copy()
 for i in range(DWT_recursion_level):
-    A, H, V, D = DWT(A)
-    
-    im[1 + i][0].imshow(A)
-    im[1 + i][0].set_title("Approximation Coeff " + str(i + 1))
-    im[1 + i][1].imshow(H)
-    im[1 + i][1].set_title("Horizontal " + str(i + 1))
-    im[1 + i][2].imshow(V)
-    im[1 + i][2].set_title("Vertical " + str(i + 1))
-    im[1 + i][3].imshow(D)
-    im[1 + i][3].set_title("Diagonal " + str(i + 1))
+    y, u, v = DWT(y, u, v)
 
 ##############################################################################################
 # 1D array
 ##############################################################################################
-A = A.astype(int)
 oneDimensionalImage = []
-width = len(A)
-height = len(A[0])
-for column in range(width):
-    for row in range(height):
-        for color in range(3):
-            oneDimensionalImage.append(A[column][row][color])
+
+y = y.astype(int)
+u = u.astype(int)
+v = v.astype(int)
+
+for column in range(len(y)):
+    for row in range(len(y[0])):
+        oneDimensionalImage.append(y[column][row])
+for column in range(len(u)):
+    for row in range(len(u[0])):
+        oneDimensionalImage.append(u[column][row])
+for column in range(len(v)):
+    for row in range(len(v[0])):
+        oneDimensionalImage.append(v[column][row])
 
 ##############################################################################################
 #  1D array to LZW
@@ -143,8 +114,6 @@ for i in range(1,len(oneDimensionalImage)):
         dictsymb += [oneDimensionalImage[i]]
         dictbin += ["{:b}".format(nbsymboles)] 
         nbsymboles +=1
-        
-longueurOriginale = np.ceil(np.log2(nbsymboles))*len(oneDimensionalImage)    
 
 for i in range(nbsymboles):
     dictbin[i] = "{:b}".format(i).zfill(int(np.ceil(np.log2(nbsymboles))))
@@ -185,6 +154,7 @@ print("Taux de compression = {0}".format(1 - longueur / longueurOriginale))
 ##############################################################################################
 #  LWZ to 1D array
 ##############################################################################################
+decoded_1D_image = []
 #TODO
 
 
@@ -203,12 +173,11 @@ print("Taux de compression = {0}".format(1 - longueur / longueurOriginale))
 ##############################################################################################
 #  YUV to RGB
 ##############################################################################################
-img_RGB = convertImageToRGB(A.copy())
+img_RGB = convertImageToRGB(y, u, v)
 im[0][2].imshow(img_RGB)
 im[0][2].set_title("Image RGB")
 
 ##############################################################################################
 #  plt 
 ##############################################################################################
-fig.delaxes(im[0][3])
 plt.show()
