d59c8e2b7d0f71224f4fc4a0f0c30d18f273d1a5
fx46 <31199346+fx46@users.noreply.github.com>
2020-12-05 17:07:55 -0500

    Finished algo bob amélioré

--- TP3/main.py
+++ TP3/main.py
@@ -6,37 +6,85 @@ import matplotlib.pyplot as plt
 import xlwt
 
 ###########################################################################
-# Recherche d'une image parmis les frames pour obtenir la vidéo
+# Bob amélioré
 ###########################################################################
-#
-#start_time = time.time()
-#
-#img = cv2.imread('data/png/i001.png') 
-#
-#allTimeLow = 1000
-#nameAllTimeLow = ''
-#
-#for filename in os.listdir('data/video/frames'):
-#    frame = cv2.imread('data/video/frames/' + filename) 
-#
-#    if sum(img.mean(axis=0).mean(axis=0)) > sum(frame.mean(axis=0).mean(axis=0)):
-#        result = cv2.subtract(img, frame)
-#    else: 
-#        result = cv2.subtract(frame, img)
-#    average_result = result.mean(axis=0).mean(axis=0)
-#
-#    brightness = sum(average_result)
-#
-#    if brightness < allTimeLow:
-#        allTimeLow = brightness
-#        print(allTimeLow)
-#        nameAllTimeLow = filename
-#
-#print("done!")
-#print("At video: " + nameAllTimeLow)
-#print("--- %s seconds ---" % (time.time() - start_time))
-#
-#
+
+book = xlwt.Workbook(encoding="utf-8")
+sheet = book.add_sheet("sheet")
+
+for imgNum in range(1, 201):
+    print("finding image: " + str(imgNum))
+
+    allTimeLow = 10000
+    start_time = time.time()
+
+    if imgNum < 10:
+        img = cv2.imread('data/png/i00' + str(imgNum) + '.png') 
+    elif imgNum < 100:
+        img = cv2.imread('data/png/i0' + str(imgNum) + '.png') 
+    else:
+        img = cv2.imread('data/png/i' + str(imgNum) + '.png') 
+
+
+    for filename in os.listdir('data/video/frames'):
+        frame = cv2.imread('data/video/frames/' + filename) 
+
+        if sum(img.mean(axis=0).mean(axis=0)) > sum(frame.mean(axis=0).mean(axis=0)):
+            result = cv2.subtract(img, frame)
+        else: 
+            result = cv2.subtract(frame, img)
+        average_result = result.mean(axis=0).mean(axis=0)
+
+        brightness = sum(average_result)
+
+        if brightness < allTimeLow:
+            allTimeLow = brightness
+            nameAllTimeLow = filename
+
+    video_path = 'data/video/' + nameAllTimeLow.split("_")[0] + ".mp4"
+    print("Analyzing video: " + video_path)
+
+    vidcap = cv2.VideoCapture(video_path)
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    frameNum = 0
+    found = False
+
+    while True:
+        frameNum = frameNum + 1
+        success, frame = vidcap.read()
+
+        if not success:
+            #fin de video
+            break
+
+        if sum(img.mean(axis=0).mean(axis=0)) > sum(frame.mean(axis=0).mean(axis=0)):
+            result = cv2.subtract(img, frame)
+        else: 
+            result = cv2.subtract(frame, img)
+        average_result = result.mean(axis=0).mean(axis=0)
+        brightness = sum(average_result)
+
+        if brightness < allTimeLow:
+            allTimeLow = brightness
+            if allTimeLow <= 1.0: # close enough to zero, use bigger number (like 30) when using jpegs
+                found = True
+                sheet.write(imgNum, 0, "%s" % (time.time() - start_time))
+                sheet.write(imgNum, 1, video_path.split(".")[0].split("/")[2])
+                sheet.write(imgNum, 2, str(frameNum / fps))
+                print("done!")
+                print("At frame: " + str(frameNum) + ", at second: " + str(frameNum / fps))
+                print("--- %s seconds ---" % (time.time() - start_time))
+                break
+
+    if not found:
+        print("not found")
+        sheet.write(imgNum, 0, "%s" % (time.time() - start_time))
+        sheet.write(imgNum, 1, "not found")
+
+book.save("tempsBobAmeliore.xls")
+
+
+
 ###########################################################################
 # Méthode pour sauvegarder des frames d'une vidéo (pour faire comme BOB, 
 # enlever numWantedCaptures et juste enregistrer chaque frames.
@@ -154,69 +202,69 @@ import xlwt
 ###########################################################################
 # Alice
 ###########################################################################
-
-book = xlwt.Workbook(encoding="utf-8")
-sheet = book.add_sheet("sheet")
-
-for imgNum in range(1, 201):
-    print("finding image: " + str(imgNum))
-
-    if imgNum < 10:
-        img = cv2.imread('data/png/i00' + str(imgNum) + '.png') 
-    elif imgNum < 100:
-        img = cv2.imread('data/png/i0' + str(imgNum) + '.png') 
-    else:
-        img = cv2.imread('data/png/i' + str(imgNum) + '.png') 
-
-    found = False
-    allTimeLow = 10000
-    start_time = time.time()
-
-    for vidNum in range(1, 51):
-        print("Analyzing video: " + str(vidNum))
-
-        if vidNum < 10:
-            vidcap = cv2.VideoCapture('data/video/v0' + str(vidNum) + '.mp4')
-        else:
-            vidcap = cv2.VideoCapture('data/video/v' + str(vidNum) + '.mp4')
-            
-        fps = vidcap.get(cv2.CAP_PROP_FPS)
-        frameNum = 0
-
-        while True:
-            frameNum = frameNum + 1
-            success, frame = vidcap.read()
-
-            if not success:
-                #fin de video
-                break
-
-            if sum(img.mean(axis=0).mean(axis=0)) > sum(frame.mean(axis=0).mean(axis=0)):
-                result = cv2.subtract(img, frame)
-            else: 
-                result = cv2.subtract(frame, img)
-            average_result = result.mean(axis=0).mean(axis=0)
-            brightness = sum(average_result)
-
-            if brightness < allTimeLow:
-                allTimeLow = brightness
-                if allTimeLow <= 1.0: # close enough to zero, use bigger number (like 30) when using jpegs
-                    sheet.write(imgNum, 0, "%s" % (time.time() - start_time))
-                    sheet.write(imgNum, 1, "video: " + str(vidNum))
-                    sheet.write(imgNum, 2, "minutage: " + str(frameNum / fps))
-                    found = True
-                    print("done!")
-                    print("At frame: " + str(frameNum) + ", at second: " + str(frameNum / fps))
-                    print("--- %s seconds ---" % (time.time() - start_time))
-                    break
-
-        if found:
-            break
-
-    if not found:
-        print("not found!")
-        print("--- %s seconds ---" % (time.time() - start_time))
-        sheet.write(imgNum, 0, "%s" % (time.time() - start_time))
-        sheet.write(imgNum, 1, "video: " + str(vidNum))
-
-book.save("tempsAlice.xls")
+#
+#book = xlwt.Workbook(encoding="utf-8")
+#sheet = book.add_sheet("sheet")
+#
+#for imgNum in range(1, 201):
+#    print("finding image: " + str(imgNum))
+#
+#    if imgNum < 10:
+#        img = cv2.imread('data/png/i00' + str(imgNum) + '.png') 
+#    elif imgNum < 100:
+#        img = cv2.imread('data/png/i0' + str(imgNum) + '.png') 
+#    else:
+#        img = cv2.imread('data/png/i' + str(imgNum) + '.png') 
+#
+#    found = False
+#    allTimeLow = 10000
+#    start_time = time.time()
+#
+#    for vidNum in range(1, 51):
+#        print("Analyzing video: " + str(vidNum))
+#
+#        if vidNum < 10:
+#            vidcap = cv2.VideoCapture('data/video/v0' + str(vidNum) + '.mp4')
+#        else:
+#            vidcap = cv2.VideoCapture('data/video/v' + str(vidNum) + '.mp4')
+#            
+#        fps = vidcap.get(cv2.CAP_PROP_FPS)
+#        frameNum = 0
+#
+#        while True:
+#            frameNum = frameNum + 1
+#            success, frame = vidcap.read()
+#
+#            if not success:
+#                #fin de video
+#                break
+#
+#            if sum(img.mean(axis=0).mean(axis=0)) > sum(frame.mean(axis=0).mean(axis=0)):
+#                result = cv2.subtract(img, frame)
+#            else: 
+#                result = cv2.subtract(frame, img)
+#            average_result = result.mean(axis=0).mean(axis=0)
+#            brightness = sum(average_result)
+#
+#            if brightness < allTimeLow:
+#                allTimeLow = brightness
+#                if allTimeLow <= 1.0: # close enough to zero, use bigger number (like 30) when using jpegs
+#                    sheet.write(imgNum, 0, "%s" % (time.time() - start_time))
+#                    sheet.write(imgNum, 1, "video: " + str(vidNum))
+#                    sheet.write(imgNum, 2, "minutage: " + str(frameNum / fps))
+#                    found = True
+#                    print("done!")
+#                    print("At frame: " + str(frameNum) + ", at second: " + str(frameNum / fps))
+#                    print("--- %s seconds ---" % (time.time() - start_time))
+#                    break
+#
+#        if found:
+#            break
+#
+#    if not found:
+#        print("not found!")
+#        print("--- %s seconds ---" % (time.time() - start_time))
+#        sheet.write(imgNum, 0, "%s" % (time.time() - start_time))
+#        sheet.write(imgNum, 1, "video: " + str(vidNum))
+#
+#book.save("tempsAlice.xls")
